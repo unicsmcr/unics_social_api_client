eee35ee533df8ba17ef9d42e69085fd483c3ea61
Sahil Borkar <sahilsborkar@gmail.com>
2020-09-09 00:51:33 +0100

    fix: allowed authenticate tests to check validity of info passed

--- src/__tests__/authenticate.test.ts
+++ src/__tests__/authenticate.test.ts
@@ -12,12 +12,12 @@ afterEach(() => {
 const apiClientInit = new apiClient.APIClient({ apiBase: 'unics-social' });
 
 test('authenticate(): authenticates valid users correctly', async () => {
-	mock.onPost('unics-social/authenticate').reply(200, { token: tokenAuth });
+	mock.onPost('unics-social/authenticate', validData[0]).reply(200, { token: tokenAuth });
 	const tokenCheck = await apiClientInit.authenticate(validData[0]);
 	expect(tokenCheck).toEqual(tokenAuth);
 });
 
 test('authenticate(): throws error if user enters invalid credentials', async () => {
-	mock.onPost('unics-social/authenticate').reply(400, { token: tokenAuth });
+	mock.onPost('unics-social/authenticate', invalidData[0]).reply(400, { token: tokenAuth });
 	await expect(apiClientInit.authenticate(invalidData[0])).rejects.not.toEqual(tokenAuth);
 });
