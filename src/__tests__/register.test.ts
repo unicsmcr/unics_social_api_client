d6daf86138c22ffcd16e4777b15bbf55e797c057
graphtheoryiscool <kashish.raimalani@student.manchester.ac.uk>
2020-09-09 11:18:09 +0530

    edited onPost func

--- src/__tests__/register.test.ts
+++ src/__tests__/register.test.ts
@@ -17,30 +17,30 @@ afterEach(() => {
 
 
 test('register(): registers user with valid data', async () => {
-	mock.onPost('unics_social/register').reply(204, {});
 	for (const fixture of validData) {
+		mock.onPost('unics_social/register', fixture).reply(204, {});
 		const response = await myAPIClient.register(fixture);
 		expect(response.data).toEqual({});
 	}
 });
 
 test('register(): throws error if user enters invalid email', async () => {
-	mock.onPost('unics_social/register').reply(400, {
-		status: 400,
-		error: 'A University of Manchester student email account is required'
-	});
 	for (const fixture of invalidEmail) {
+		mock.onPost('unics_social/register', fixture).reply(400, {
+			status: 400,
+			error: 'A University of Manchester student email account is required'
+		});
 		await expect(getResponseBody(myAPIClient.register(fixture))).rejects.toMatchObject({ error: 'A University of Manchester student email account is required' });
 	}
 });
 
 
 test('register(): throws error if password is too small', async () => {
-	mock.onPost('unics_social/register').reply(400, {
-		status: 400,
-		error: 'Password must be at least 10 characters long'
-	});
 	for (const fixture of invalidPassword) {
+		mock.onPost('unics_social/register', fixture).reply(400, {
+			status: 400,
+			error: 'Password must be at least 10 characters long'
+		});
 		await expect(getResponseBody(myAPIClient.register(fixture))).rejects.toMatchObject({ error: 'Password must be at least 10 characters long' });
 	}
 });
