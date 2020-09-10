7d5739cc49f1fe3a110326c0702f53cb941c483f
Sahil Borkar <57853382+sahilsborkar@users.noreply.github.com>
2020-09-10 15:22:24 +0100

    test: authenticate() and getUser() (#14)
    * feat: added fixtures folder and getUser test
    
    * test: remove fixtures from coverage
    
    * fix: changed values for users fixture
    
    * feat: added authenticate tests
    
    * fix: added changed required for PR
    
    * fix: users fixture now use proper enum values
    
    * fix: allowed authenticate tests to check validity of info passed
    
    * fix: completed requested changes
    
    * fix: removed errors
    
    Co-authored-by: Amish Shah <amishshah.2k@gmail.com>

--- src/__tests__/fixtures/users.ts
+++ src/__tests__/fixtures/users.ts
@@ -1,31 +1,26 @@
-import { APIUser, Course, Year } from '../../types/api';
+import { APIUser, Course, Year, APIAccountStatus, APIAccountType } from '../../types/api';
 export const users: APIUser[] = [
 	{
 		id: '123456',
 		forename: 'John',
 		surname: 'Doe',
-		accountStatus: 1,
-		accountType: 0,
+		accountStatus: APIAccountStatus.Verified,
+		accountType: APIAccountType.User,
 		profile: {
 			course: Course.COMPUTER_SCIENCE,
 			yearOfStudy: Year.ONE
-			// instagram: string,
-			// facebook: string,
-			// twitter: string
+
 		}
 	},
 	{
 		id: '1a2b3c4d5e',
 		forename: 'Bob',
 		surname: 'Ross',
-		accountStatus: 1,
-		accountType: 0,
+		accountStatus: APIAccountStatus.Verified,
+		accountType: APIAccountType.User,
 		profile: {
 			course: Course.ARTIFICIAL_INTELLIGENCE,
 			yearOfStudy: Year.MASTERS
-			// instagram: string,
-			// facebook: string,
-			// twitter: string
 		}
 	}
 ];
