02c6c5c9704ed5101d7c6c976185c9942c00b9ea
Sahil Borkar <sahilsborkar@gmail.com>
2020-09-10 21:39:23 +0100

    test: createEvent() added (formData presence checked, not content)

--- src/__tests__/createEvent.test.ts
+++ src/__tests__/createEvent.test.ts
@@ -1,7 +1,7 @@
 import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
 import * as apiClient from '../';
-import { validEvents, validCreateEvent, invalidCreateEvent, apiToken } from './fixtures/events';
+import { validEvents, validCreateEvent, invalidCreateEvent } from './fixtures/events';
 import FormData from 'form-data';
 
 const mock = new MockAdapter(axios);
@@ -12,20 +12,16 @@ afterEach(() => {
 
 const apiClientInit = new apiClient.APIClient({ apiBase: 'unics-social' });
 
-describe('createEvent() headers and response check', () => {
-	test('createEvent(): creates event correctly with unset contentTypeExtra', async () => {
-		const formData = new FormData();
-		for (const [key, value] of Object.entries(validCreateEvent)) {
-			formData.append(key, (key === 'image' && typeof value === 'boolean') ? String(value) : value);
-		}
-		const contentTypeExtra = '';
-
-		mock.onPost(`unics-social/events`, formData, expect.objectContaining({ 'Content-Type': `multipart/form-data;${contentTypeExtra}` })).reply(201, {
-			event: validEvents[0]
-		});
-		const createdEvent = await apiClientInit.createEvent(validCreateEvent);
-		expect(createdEvent).toEqual(validEvents[0]);
+test('createEvent(): creates event correctly with unset contentTypeExtra', async () => {
+	const formData = new FormData();
+	for (const [key, value] of Object.entries(validCreateEvent)) {
+		formData.append(key, (key === 'image' && typeof value === 'boolean') ? String(value) : value);
+	}
+	mock.onPost(`unics-social/events`, expect.any(FormData), expect.objectContaining({ 'Content-Type': expect.any(String) })).reply(201, {
+		event: validEvents[0]
 	});
+	const createdEvent = await apiClientInit.createEvent(validCreateEvent);
+	expect(createdEvent).toEqual(validEvents[0]);
 });
 
 test('createEvent(): throw no content error', async () => {
@@ -33,6 +29,6 @@ test('createEvent(): throw no content error', async () => {
 	for (const [key, value] of Object.entries(invalidCreateEvent)) {
 		formData.append(key, (key === 'image' && typeof value === 'boolean') ? String(value) : value);
 	}
-	mock.onPost(`unics-social/events`, formData).reply(204);
+	mock.onPost(`unics-social/events`, expect.any(FormData), expect.objectContaining({ 'Content-Type': expect.any(String) })).reply(204);
 	await expect(apiClientInit.createEvent(invalidCreateEvent)).rejects.toThrow();
 });
