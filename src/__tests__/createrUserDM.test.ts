1c23d8b26a46fe5ee9914a0ac6ee1e67f50df7ab
graphtheoryiscool <kashish.raimalani@student.manchester.ac.uk>
2020-09-15 21:38:34 +0530

    fixed lint"

--- src/__tests__/createrUserDM.test.ts
+++ src/__tests__/createrUserDM.test.ts
@@ -2,7 +2,7 @@ import MockAdapter from 'axios-mock-adapter';
 import { APIClient as apiClient } from '../';
 import { users } from './fixtures/users';
 import { channels } from './fixtures/channels';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import unwrapErrorBody from './util/unwrapErrorBody';
 
 const mock = new MockAdapter(axios);
@@ -23,8 +23,8 @@ test('createUserDM(): returns channel if user ID is valid', async () => {
 });
 
 test('createUserDM(): throws error if user ID is invalid', async () => {
-  for (let i = 0; i < users.length; i++) {
-		mock.onPost(`unics_social/users/${users[i].id}/channel`).reply(400, { error: "invalid user ID" });
-		await expect(unwrapErrorBody(myAPIClient.createUserDM(users[i].id))).rejects.toMatchObject( { error: "invalid user ID" });
+	for (const user of users) {
+		mock.onPost(`unics_social/users/${user.id}/channel`).reply(400, { error: 'invalid user ID' });
+		await expect(unwrapErrorBody(myAPIClient.createUserDM(user.id))).rejects.toMatchObject({ error: 'invalid user ID' });
 	}
 });
--- src/__tests__/fixtures/channels.ts
+++ src/__tests__/fixtures/channels.ts
@@ -1,11 +1,12 @@
 import { APIChannel } from '../../types/api';
+
 export const channels: APIChannel[] = [
 	{
-    id: '123',
-  	lastUpdated: '1a2b3c'
+		id: '123',
+		lastUpdated: '1a2b3c'
 	},
 	{
-		id: '1a2b3c4d5e',
-		lastUpdated: '12.3.4'
+		id: '3.14159',
+		lastUpdated: '12.03.12'
 	}
 ];
